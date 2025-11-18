7f566002d458ea44217cd944e81d79069659c799
Catcus-p <chhetribarsha537@gmail.com>
2025-11-18 09:31:40 +0545

    accessing types

--- labpackage2/admin.java
+++ labpackage2/admin.java
@@ -1,8 +1,9 @@
 package labpackage2;
 import package1.student;
-public class admin{
+public class admin extends student{
     public static void main(String[]args){
         student s = new student();
         s.diplay();
+        System.out.println("accessing protected b="+s.c);
     }
 }
--- package1/student.java
+++ package1/student.java
@@ -2,7 +2,13 @@ package package1;
  public class student{
     private int a =10;
     protected int b=20;
+    public int c=30;
+    int d=30;
     public void diplay(){
         System.out.println("i am in student class");
+        System.out.println("private a="+a);
+        System.out.println("protected b="+b);
+        System.out.println("public c="+c);
+        System.out.println("a="+d);
     }
-}
+    }
